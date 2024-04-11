ca4567680bf8bbee9d6a466a6f8c126a24d3264b
aviv0404 <aviv040400@gmail.com>
2024-04-11 18:02:53 +0300

    remade snort without turn issues now (not completely done)

---
+++ Snort.py
@@ -0,0 +1,153 @@
+import numpy
+
+
+class Snort:
+    EMPTY, BLACK, RED, BLUE, ONGOING = 0, 1, 2, 3, 4
+
+    def __init__(self, starting_player, board_size=10, num_black_squares=3) -> None:
+        self.board_size = board_size
+
+        self.current_player = starting_player
+        self.board = numpy.zeros((board_size, board_size))
+
+        # setup legal moves for each player
+        self.red_legal_moves = numpy.ones(
+            (self.board_size, self.board_size), dtype=numpy.bool_
+        )
+        self.blue_legal_moves = numpy.ones(
+            (self.board_size, self.board_size), dtype=numpy.bool_
+        )
+        self.move_history = []
+
+        self.init_black_squares(num_black_squares)
+
+    def init_black_squares(self, num_black_squares):
+        black_squares_filled = 0
+        while black_squares_filled < num_black_squares:
+            # get random move
+            random_move = (
+                numpy.random.randint(0, self.board_size),
+                numpy.random.randint(0, self.board_size),
+            )
+
+            # insert random move to the board
+            if self.board[random_move] == self.EMPTY:
+
+                self.board[random_move] = self.BLACK
+
+                # update legal moves
+                self.red_legal_moves[random_move] = False
+                self.blue_legal_moves[random_move] = False
+
+                # take care of neighbors
+                neighbors = self._get_cell_neighbors(random_move)
+                for neighbor in neighbors:
+                    self.red_legal_moves[neighbor] = False
+                    self.blue_legal_moves[neighbor] = False
+
+                black_squares_filled += 1
+
+    # returns locations of all the neighbors
+    # of the given cell in the board matrix
+    def _get_cell_neighbors(self, cell: tuple):
+        # returns all the neighbors of a cell, regardless of which player is in these squares
+        #
+        row, column = cell
+        neighbors = []
+        # up
+        if row - 1 > 0:
+            neighbors.append((row - 1, column))
+
+        # down
+        if row + 1 < self.board_size:
+            neighbors.append((row + 1, column))
+
+        # left
+        if column - 1 > 0:
+            neighbors.append((row, column - 1))
+
+        # right
+        if column + 1 < self.board_size:
+            neighbors.append((row, column + 1))
+
+        return neighbors
+
+    # returns the player opposite of the one given as a parameter
+    def other_player(self, player):
+        if player == self.RED:
+            return self.BLUE
+        else:
+            return self.RED
+
+    # switches the player from RED to BLUE or
+    # from BLUE to RED depends on who's playing right now
+    def _switch_player(self):
+        self.current_player = self.other_player(self.current_player)
+
+    # makes the move on the board and
+    # returns True if the move was made
+    # and False otherwise
+    def make_move(self, move: tuple):
+        if self.is_legal_move(move):
+            self.board[move] = self.current_player
+            self._update_legal_moves(move)
+            self.move_history.append(move)
+            self._switch_player()
+            return True
+
+        return False
+
+    # gets the move and updates red or blue legal moves
+    def _update_legal_moves(self, move: tuple):
+        neighbors = self._get_cell_neighbors(move)
+
+        # get the correct legal moves array
+        legal_moves = self._get_legal_moves(self.other_player(self.current_player))
+
+        # update legal moves
+        legal_moves[move] = False
+        for neighbor in neighbors:
+            legal_moves[neighbor] = False
+
+    # clones this game and returns the new object
+    def copy(self):
+        result = Snort(self.board_size)
+        result.current_player = self.current_player
+        # self.board =
+        pass
+
+    # returns either RED, BLUE or ONGOING to tell either
+    # who won or if the game is still ongoing
+    def outcome(self):
+        if numpy.all(self.blue_legal_moves == False):
+            return self.RED
+
+        elif numpy.all(self.red_legal_moves == False):
+            return self.BLUE
+        else:
+            return self.ONGOING
+
+    # returns the last move that was made
+    def last_move(self):
+        if len(self.move_history) == 0:
+            return None
+        return self.move_history[-1]
+
+    # returns a list of available moves for the current player
+    def _get_legal_moves(self, player):
+        return self.red_legal_moves if player == self.RED else self.blue_legal_moves
+
+    # checks if the given move is a move the current player can play
+    def is_legal_move(self, move: tuple):
+        row, column = move
+        if (
+            # not an empty slot
+            self.board[move] != self.EMPTY
+            # out of bounds
+            or (row < 0 or row >= self.board_size)
+            or (column < 0 or column >= self.board_size)
+        ):
+            return False
+
+        legal_moves = self._get_legal_moves(self.current_player)
+        return legal_moves[move]
